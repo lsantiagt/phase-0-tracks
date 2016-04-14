de9bc020eeaf318a956546f05ae309070a261481
Luis Santiago <luis.santiago@fwpr.com>
2016-04-14 08:03:22 -0400

    Completed code for pairing session adding logic for printing to console results of data imputed by user.

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,7 +1,7 @@
 puts "Hamster's Name:"
 name = gets.chomp
 
-puts "Hamster Volume Level"
+puts "Hamster Volume Level (1 to 10)"
 volume_level = gets.chomp.to_i
 
 puts "Hamster's Fur Color:"
@@ -16,10 +16,35 @@ else
   good_candidate = false
 end
 
-puts "Age:"
+puts "Age in days:"
 age = gets.chomp
 if age.empty?
    age = nil
 else
    age = age.to_i
+end
+
+puts "You have register #{name} with volume level #{volume_level} and the fur color is #{fur_color}. ;"
+
+if age == nil
+   puts "The age of the hamster is unknown."
+else
+   puts "The age of the hamster is #{age}."
+end
+
+if good_candidate
+   puts "Is a good candidate for adoption"
+else
+   puts "Is not a good candidate for adoptions"
+end
+
+case volume_level
+when 0,1,2,3
+    puts "Is silent!"
+when 4,5,6
+    puts "Is not that noicy!"
+when 7,8,9,10
+    puts "Is really noicy!!"
+else
+    puts "Our guess is, that is really silent!! "
 end

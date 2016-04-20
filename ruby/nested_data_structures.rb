a866ace817a5bf02acb55ca6605ea8cea399e731
Luis Santiago <luis.santiago@fwpr.com>
2016-04-19 22:16:16 -0400

    Updates to Nested data to add another level

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,37 +1,56 @@
 highway = {
     sport_cars: {
-        make: 'Porsche 911',
-        model: '911',
-        seat_info: {
-            total_seats: 2,
-            seats_available: 1
-        },
-        ocupant: [
-            "Jorge"        ]
-    },
+        sport1: {  
+           make: 'Porsche 911',
+           model: '911',
+           seat_info: {
+               total_seats: 2,
+               seats_available: 1
+                      },
+           ocupant: [
+              "Jorge"        ]
+                 } ,
+                } ,
     sedan: {
-        make:  'Ford',
-        model: 'Fusion',
-        seat_info: {
-            total_seats: 5,
-            seats_available: 3
-        },
-        ocupant: [
-            "Marisol", 
-            "Adriana"
-        ]
-    },
+        car1: {
+           make:  'Ford',
+           model: 'Fusion',
+           seat_info: {
+              total_seats: 5,
+              seats_available: 3
+                       },
+           ocupant: [
+               "Marisol", 
+               "Adriana"
+                    ]
+               } ,
+             }  ,
     minivan: {
-        make: 'Honda',
-        model: 'Oddessey',
-        seat_info: {
-            total_seats: 7,
-            seats_available: 3
-        },
-        ocupant: ["Luis","Mariella","Rosa"]
-    }
+        van1: {
+            make: 'Honda',
+            model: 'Oddessey',
+            seat_info: {
+                total_seats: 7,
+                seats_available: 3
+                       },
+            ocupant: ["Luis","Mariella","Rosa"]
+              },
+        van2: {
+            make: 'Toyota',
+            model: 'Sienna',
+            seat_info: {
+                total_seats: 7,
+                seats_available: 6
+                       },
+            ocupant: ["Adriana"]
+              },
+             } 
 }
 
-p highway[:minivan][:model]
+p highway[:minivan][:van1][:model]
 
-p highway[:minivan][:make]
+p highway[:minivan][:van1][:make]
+
+p highway[:minivan][:van2][:ocupant][0]
+
+p highway[:sport_cars][:sport1][:seat_info][:seats_available]

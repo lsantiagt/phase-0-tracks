d37cd7634ab802bdc393468cbaf89a2f49f783a6
Luis Santiago <luis.santiago@fwpr.com>
2016-04-20 22:31:14 -0400

    Added comments to code

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,47 +1,52 @@
+# Alias_manager.rb
+# Program to convert names to aliases and print output
+#
+
+# Find next vowel method
 def next_vowel(n1)
-	   vlist = 'aeiou'.chars
-	   #p vlist
-	   newval = ""
-	   for x in 0..n1.length.to_i - 1  
+   vlist = 'aeiou'.chars
+   #p vlist
+   newval = ""
+   for x in 0..n1.length.to_i - 1  
 
-	       if vlist.include? n1[x]
-	       	       #p vlist.index(n1[x])
-	       	       #p vlist.size
-	            if vlist.index(n1[x]) == vlist.size - 1
-	            newval << vlist[0] 
-	            else
-	            newval << vlist[vlist.index(n1[x]) + 1]       
-	            end
-	          
-	        else
-	          if n1[x] == nil
-	          	 newval << " "
-	          else	 
-	             newval << n1[x]
-	          end
-	       end
-	       x += 1
-	   end
-	   return newval
-	end
+       if vlist.include? n1[x]
+       	       #p vlist.index(n1[x])
+       	       #p vlist.size
+            if vlist.index(n1[x]) == vlist.size - 1
+            newval << vlist[0] 
+            else
+            newval << vlist[vlist.index(n1[x]) + 1]       
+            end
+          
+        else
+          if n1[x] == nil
+          	 newval << " "
+          else	 
+             newval << n1[x]
+          end
+       end
+       x += 1
+   end
+   return newval
+end
 
-	def next_cons(n2)
-	     newconst=""
-	     vowels = ["a","e","i","o","u"," "]
-	    for i in 0..n2.length.to_i - 1  
-	        if vowels.include? n2[i]
-	           newconst << n2[i]
-	        else   	
-	           if vowels.include? n2[i].downcase.next
-	       	      newconst << n2[i].next.next
-	       	   else   
-	       	      newconst << n2[i].next
-	       	   end   
-	        end	  
-	     end  	
-	     return newconst
-	end	
-
+# Method to get next constant
+def next_cons(n2)
+     newconst=""
+     vowels = ["a","e","i","o","u"," "]
+    for i in 0..n2.length.to_i - 1  
+        if vowels.include? n2[i]
+           newconst << n2[i]
+        else   	
+           if vowels.include? n2[i].downcase.next
+       	      newconst << n2[i].next.next
+       	   else   
+       	      newconst << n2[i].next
+       	   end   
+        end	  
+     end  	
+     return newconst
+end	
 
 agent_list = []
 alias_list = []
@@ -58,7 +63,6 @@ while vexit == 1 do
 	    name = inname.split(' ')
 		name = name[-1] + " " + name[0]
 		p name
-
 		
 	    agent_list << name
 		agent_alias = next_vowel(name.chars)
@@ -69,7 +73,7 @@ while vexit == 1 do
     end
    
 end
-
+# Print all inputs
 for i in 0..agent_list.size - 1
       puts "#{agent_list[i]} is also known as #{alias_list[i]}"
 end

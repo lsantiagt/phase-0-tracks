4b5ea0df5e77e997d14b154a1280e7d8464b7e85
Luis Santiago <luis.santiago@fwpr.com>
2016-04-27 15:17:10 -0400

    Create new class and instances as per instance methods session

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,66 @@ class Puppy
     toy
   end
 
+  def speak(num)
+      num.times {puts "Woof!"}
+  end
+
+  def roll_over
+  	  puts "rolls over"
+  end	  
+
+  def dog_years(year)
+      dy = year * 7
+      puts dy
+  end 	
+
+  def sit_down
+    puts "sits down"
+  end
+
+  def inicialize
+    puts "Initializing new puppy instance..."
+  end
+
 end
 
+mydog = Puppy.new
+mydog.inicialize 
+#mydog.initialize does not work since initialize is a builtin method of 
+mydog.fetch("ball")
+mydog.speak(5)
+mydog.roll_over
+mydog.dog_years(7)
+
+
+
+class Car_maintenance
+  
+    def initialize
+      puts "Initializing groomers instance..."
+    end
+
+    def oil_change
+      puts "The oil change done!"
+    end
+
+    def brake_inspection
+      puts "The brakes are ok!"
+    end
+
+end
+
+Maintenance_visits = []
+
+50.times do
+  puts "Visits "
+  Maintenance_visits << Car_maintenance.new 
+end
+
+p Maintenance_visits
+
+# iterate through using .each
+Maintenance_visits.each do |visit|
+  visit.oil_change
+  visit.brake_inspection
+end

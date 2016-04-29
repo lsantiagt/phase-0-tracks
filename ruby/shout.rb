b208fc7c48ae99ac057126c11221f7c9930f4bbd
Luis Santiago <luis.santiago@fwpr.com>
2016-04-29 13:00:31 -0400

    Add calls to methods within a module, creating a class to include the module and accesing the class.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,32 @@
+# module Shout
+#   # we'll put some methods here soon, but this code is fine for now!
+
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yell_happily(words)
+#     words + "!!!" + " :)"
+#   end
+# end
+
+# puts Shout.yell_happilly("Hamburgers")
+
+
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+  def yell_angrily(words)
+    words + "!!!" + " :("
+  end
+  def yell_hapilly(words)
+    words + "!!!" + " :)" 
+  end
 end
+
+class Emotions
+  include Shout
+end
+
+
+
+puts Emotions.new.yell_angrily("Stop yelling at me!")
+puts Emotions.new.yell_hapilly("Let's party on tonight")

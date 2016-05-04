63eac98a3abd381f4fa6c04a3e2e494e41be5482
Luis Santiago <luis.santiago@fwpr.com>
2016-05-04 17:24:48 -0400

    My first js program, created a function to reverse a string and did an if statement and print output

---
+++ js/explore.js
@@ -0,0 +1,23 @@
+// Create reverse funcion
+// Take string inputted by the user
+// need to find the length of the string
+// 
+// for each letter in the string from the last letter
+// insert into a new variable and continue appending to it
+
+//Variables
+var i = 0;
+var newstr = "";
+var reversedword = "";
+
+
+function  reverse(string) { 
+   for (i = string.length - 1; i >=  0; i-- ) 
+       newstr = newstr + string[i];
+       return newstr 
+ }
+
+reversedword = (reverse("Hello!"));
+if (1==1) {
+   console.log( reversedword );
+}

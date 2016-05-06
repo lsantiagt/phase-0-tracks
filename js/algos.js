bb2d6d8a95ac5ffd510328ebbe4c8c9d7727714c
Luis Santiago <luis.santiago@fwpr.com>
2016-05-05 23:42:31 -0400

    Added logic to validate if a key-value repeates in two hashes

--- js/algos.js
+++ js/algos.js
@@ -16,5 +16,49 @@ function longest_word(word_array) {
     }
 }
 
-// 3
+// Write a function that
+// Receive two hashes as parameters
+// 1. Check each key-value on hash 1 and verify if exists in hash 2
+// 2. if any key-value exists in both hash then return true
+// 3. else return false
+// 
+
+function check_keyval(hash1, hash2) {
+	
+	var v_exists = 0;
+	Object.keys(hash1).forEach(function(key, index) {
+	       //console.log(this[key]+key);
+	       // 1 
+	        Object.keys(hash2).forEach(function(key2, index2) {
+	            // 2
+	            if (hash1[key]+key == hash2[key2]+key2)  {
+	               console.log("True: " + hash1[key]+key + " " + hash2[key2]+key2);
+	               v_exists =  v_exists + 1;
+	            }   
+	            else {
+	            	console.log("False: "  + hash1[key]+key + " "  + hash2[key2]+key2);
+	            }
+	        },hash1)
+
+	  });
+	//3
+     if (v_exists > 0) {return true;} else {return false;}
+}
+
+
+// 3 function longest word
 longest_word(["long phrase","longest phrase","longer phrase"])
+
+var hash1={name: "Steven", age: 54, country: "USA"};
+var hash2={name: "Tamir", age: 54};
+var hash3={name: "Luis", age: 53, country: "USA"};
+var check_results = null;
+console.log("");
+check_results = check_keyval(hash1, hash2);
+console.log("First result is " + check_results );
+console.log("");
+check_results = check_keyval(hash1, hash3);
+console.log("First result is " + check_results);
+console.log("");
+check_results = check_keyval(hash2, hash3);
+console.log("First result is " + check_results);

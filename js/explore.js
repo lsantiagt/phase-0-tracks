e3cd75d56aafdca07a78ee38f7ec67d1a3bb1c44
Luis Santiago <luis.santiago@fwpr.com>
2016-05-09 13:15:09 -0400

    Some refactor on variables

--- js/explore.js
+++ js/explore.js
@@ -1,18 +1,12 @@
-// Create reverse funcion
 // Take string inputted by the user
 // need to find the length of the string
 // 
 // for each letter in the string from the last letter
 // insert into a new variable and continue appending to it
-
 //Variables
 
-var i = 0;
-var newstr = "";
-var reversedword = "";
-
-
-function  reverse(string) { 
+function reverse(string) { 
+   var newstr = "";
    for (i = string.length - 1; i >=  0; i-- ) 
        newstr = newstr + string[i];
        return newstr 
